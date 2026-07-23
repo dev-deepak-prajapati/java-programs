1036d18835286beb088cd71e56aa0ab1ac73dc7d
dev-deepak-prajapati <dev.deepak.prajapati@gmail.com>
2026-07-23 17:57:52 +0530

    linked list JCF

---
+++ collectionframwork/LLColl.java
@@ -0,0 +1,30 @@
+/**
+ * @code This Program has been done by
+ * @author ☠ Hacker's ☠ Deepak Prajapati️
+ * @see Mobile : 8810839406
+ *
+ */
+package collectionframwork;
+
+import java.util.LinkedList;
+
+public class LLColl {
+
+    public static void main(String[] args) {
+        LinkedList<String> li = new LinkedList<>();
+        li.add("Apple");
+        li.add("Boy");
+        li.add("Cat");
+        li.add("Dog");
+        System.out.println(li);
+        li.addFirst("Elephant");
+        System.out.println(li);
+        li.addLast("anjir");
+        System.out.println(li);
+        li.removeFirst();
+        System.out.println(li);
+        li.removeLast();
+        System.out.println(li);
+
+    }
+}
